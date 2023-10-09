6446e1ff9bfbd3a61cba1866f688d6355e9b5fcc
TendaniMamadi <mamadi.tendani@gmail.com>
2023-10-09 16:10:58 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+//navbar control
